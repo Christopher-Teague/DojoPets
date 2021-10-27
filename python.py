34d0df3455584b6f9ccbece8563c5f771b88932a
Christopher-Teague <cmteague77@gmail.com>
2021-10-27 16:43:22 -0700

    origin

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 
 class Ninja:
-    def __init__ (self, first_name , last_name , treats , pet_food , pet ):
+    def __init__ (self, first_name , last_name, treats, pet_food, pet):
         self.first_name = first_name
         self.last_name = last_name
         self.treats = treats
@@ -8,34 +8,41 @@ class Ninja:
         self.pet = pet
     
     # implement the following methods:
-    def walk(Pet): # - walks the ninja's pet invoking the pet play() method
-        print(f"walking the {Pet}...")
-        Pet.play()
+    def walk(self): # - walks the ninja's pet invoking the pet play() method
+        self.pet.play()
 
-    def feed(Pet): # - feeds the ninja's pet invoking the pet eat() method
-        Pet.eat()
+    def feed(self): # - feeds the ninja's pet invoking the pet eat() method
+        self.pet.eat()
 
-    def bathe(Pet): # - cleans the ninja's pet invoking the pet noise() method
-        Pet.noise()
+    def bathe(self): # - cleans the ninja's pet invoking the pet noise() method
+        self.pet.noise()
 
 class Pet:
     def __init__ (self, name , type , tricks ):
         self.name = name
         self.type = type
         self.tricks = tricks
-        self.health = 100
-        self.energy = 100
+        self.health = 50
+        self.energy = 50
 
 # implement the following methods:
     def sleep(self): # - increases the pets energy by 25
         self.energy += 25
 
-    def eat(): # - increases the pet's energy by 5 & health by 10
+    def eat(self): # - increases the pet's energy by 5 & health by 10
         self.health += 10
         self.energy += 5
 
-    def play(): # - increases the pet's health by 5
+    def play(self): # - increases the pet's health by 5
+        print(f"{self.name} is being walked...")
         self.health += 5
 
-    def noise(): # - prints out the pet's sound
-        pass
+    def noise(self): # - prints out the pet's sound
+        print("Mooo")
+
+# joe = Pet("joe", "cow", "anti-tipping")
+bob = Ninja("bob", "ross", "scooby snack", "mutton", Pet("joe", "cow", "anti-tipping"))
+
+bob.walk()
+bob.feed()
+bob.bathe()
